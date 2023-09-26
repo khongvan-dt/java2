144038cf38d0fdb25172d8bff735778d717e93a3
khongvan-dt <138080989+khongvan-dt@users.noreply.github.com>
2023-09-26 11:07:48 +0700

    add

--- projectJava2/src/main/java/main/Main.java
+++ projectJava2/src/main/java/main/Main.java
@@ -7,6 +7,7 @@ import javafx.scene.Scene;
 import javafx.scene.Parent;
 import javafx.fxml.FXMLLoader;
 import java.io.IOException;
+import java.net.URL;
 
 public class Main extends Application {
 
@@ -18,26 +19,58 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+//        try {
+//            // Tạo giao diện form tạo tài khoản
+//            FXMLLoader loader = new FXMLLoader(getClass().getResource("/admin/login.fxml"));
+//            Parent root = loader.load();
+//            scene = new Scene(root); // Initialize the static scene variable
+//
+//            // Đặt CSS cho giao diện form tạo tài khoản
+//            scene.getStylesheets().add(getClass().getResource("/admin/loginCss.css").toExternalForm());
+//
+//            // Đặt độ rộng và độ dài tối thiểu của cửa sổ
+//            primaryStage.setMinWidth(905); // Độ rộng tối thiểu
+//            primaryStage.setMinHeight(1000); // Độ dài tối thiểu
+//
+//            primaryStage.setScene(scene);
+//            primaryStage.show();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
         try {
-            // Tạo giao diện form tạo tài khoản
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/admin/login.fxml"));
-            Parent root = loader.load();
-            scene = new Scene(root); // Initialize the static scene variable
+            // Lấy đường dẫn tới file FXML
+            URL fxmlUrl = getClass().getResource("/admin/login.fxml");
+            if (fxmlUrl != null) {
+                String fxmlPath = fxmlUrl.getPath();
 
-            // Đặt CSS cho giao diện form tạo tài khoản
-            scene.getStylesheets().add(getClass().getResource("/admin/loginCss.css").toExternalForm());
+                // Kiểm tra xem đường dẫn có chứa "/admin/" không
+                if (fxmlPath.contains("/admin/")) {
+                    primaryStage.setMinWidth(1300);
+                    primaryStage.setMinHeight(730);
+                }
+                if (fxmlPath.contains("/web/")) {
+                    primaryStage.setMinWidth(200);
+                    primaryStage.setMinHeight(300);
+                }
 
-            // Đặt độ rộng và độ dài tối thiểu của cửa sổ
-            primaryStage.setMinWidth(1000); // Độ rộng tối thiểu
-            primaryStage.setMinHeight(800); // Độ dài tối thiểu
+                FXMLLoader loader = new FXMLLoader(fxmlUrl);
+                Parent root = loader.load();
+                scene = new Scene(root);
 
-            primaryStage.setScene(scene);
-            primaryStage.show();
+                // Đặt CSS cho giao diện form tạo tài khoản
+                scene.getStylesheets().add(getClass().getResource("/admin/loginCss.css").toExternalForm());
+
+                // Đặt độ rộng và độ dài tối thiểu của cửa sổ
+                primaryStage.setScene(scene);
+                primaryStage.show();
+            } else {
+                System.out.println("Không tìm thấy file FXML");
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    
+
     // Chuyển trang 
     public static void setRoot(String fxml) throws IOException {
         Parent root = loadFXML(fxml);
