3633621c0597a55086e40cbd2e9e2195884b0631
khongvan-dt <138080989+khongvan-dt@users.noreply.github.com>
2023-09-28 20:48:48 +0700

    add

---
+++ projectJava2/src/main/java/models/Product.java
@@ -0,0 +1,88 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package models;
+
+/**
+ *
+ * @author thao
+ */
+public class Product {
+    Integer id;
+    Integer supplierId;
+    Integer categoryId;
+    String productName;
+    String imagePath;
+    String description;
+    Float price;
+    
+    
+    public Product(){
+        
+    }
+    
+    public Product(Integer id, String productName, String imagePath, Float price){
+        this.id = id;
+        this.productName = productName;
+        this.imagePath = imagePath;
+        this.price = price;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getSupplierId() {
+        return supplierId;
+    }
+
+    public void setSupplierId(Integer supplierId) {
+        this.supplierId = supplierId;
+    }
+
+    public Integer getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Integer categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+    
+}
