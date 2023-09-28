3a3edee4c508e26b603bf7315ad0d4fa722ecf2a
khongvan-dt <138080989+khongvan-dt@users.noreply.github.com>
2023-09-29 02:54:28 +0700

    add

--- projectJava2/src/main/java/webController/HomeController.java
+++ projectJava2/src/main/java/webController/HomeController.java
@@ -1,6 +1,7 @@
 package webController;
 
 import db.connect;
+import java.awt.Color;
 import javafx.fxml.FXML;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -31,27 +32,41 @@ public class HomeController implements Initializable {
     @FXML
     private AnchorPane bgV;
     @FXML
+    private Button categoryId1;
+    @FXML
     private ScrollPane scrollPane;
+    @FXML
+    private AnchorPane bgV1;
+    @FXML
+    private Button categoryId2;
+    @FXML
+    private ScrollPane scrollPane1;
 
     public void initialize(URL url, ResourceBundle rb) {
         System.out.println("abc");
         displayNewestProducts();
+        displayProducts2();
+
     }
 
+   
+    Connection connection = null;
+    PreparedStatement statement = null;
+    ResultSet resultSet = null;
+
     private void displayNewestProducts() {
-        Connection connection = null;
-        PreparedStatement statement = null;
-        ResultSet resultSet = null;
 
         try {
             connection = connect.getConnection();
 
-            String query = "SELECT ProductsName.ProductName, product.img, importgoods.price "
+            String query = "SELECT ProductsName.ProductName, product.img, importgoods.price, product.categoryId, category.categoryName "
                     + "FROM product "
                     + "INNER JOIN ProductsName ON product.ProductNameId = ProductsName.ProductNameId "
                     + "INNER JOIN importgoods ON importgoods.ProductNameId = product.ProductNameId "
-                    + "INNER JOIN supplier ON product.supplier_id = supplier.supplierId ";
-//             + "LIMIT 4 OFFSET 0"
+                    + "INNER JOIN supplier ON product.supplier_id = supplier.supplierId "
+                    + "INNER JOIN category ON product.categoryId = category.categoryId "
+                    + "WHERE product.categoryId = 10";
+
             statement = connection.prepareStatement(query);
 
             resultSet = statement.executeQuery();
@@ -63,6 +78,12 @@ public class HomeController implements Initializable {
             int startY = 4; // Vị trí ban đầu theo trục Y
 
             int productIndex = 0; // Chỉ số của sản phẩm
+            if (resultSet.next()) {
+                String categoryName = resultSet.getString("categoryName");
+
+                // Đặt giá trị categoryName cho nút Button
+                categoryId1.setText(categoryName);
+            }
 
             while (resultSet.next()) {
                 String productName = resultSet.getString("ProductName");
@@ -82,18 +103,15 @@ public class HomeController implements Initializable {
                 Image image = new Image("file:///C:/java2/projectJava2/" + imagePath);
                 productImageView.setImage(image);
 
-// Tạo Button thay vì Label cho tên sản phẩm
-                Button productNameButton = new Button(productName);
-                productNameButton.setPrefWidth(150);
-                productNameButton.setPrefHeight(25);
-                productNameButton.setAlignment(Pos.CENTER);
+//// Tạo Label cho tên sản phẩm
+                Label productNameLabel = new Label(productName);
+                productNameLabel.setPrefWidth(150);
+                productNameLabel.setPrefHeight(25);
+                productNameLabel.setAlignment(Pos.CENTER);
 
-// Đặt lớp CSS cho Button để đặt màu nền thành màu đỏ
-//                productNameButton.getStyleClass().add("button-with-red-background");
-
-// Đặt xử lý sự kiện cho Button (nếu cần)
-                productNameButton.setOnAction(event -> {
-                    // Điều khiển hành động khi Button được nhấp
+                // Đặt xử lý sự kiện cho Label 
+                productNameLabel.setOnMouseClicked(event -> {
+                    // Xử lý khi Label được nhấp chuột
                 });
 
 // Đưa Button và các thành phần khác vào VBox
@@ -104,7 +122,7 @@ public class HomeController implements Initializable {
                 productPriceLabel.setAlignment(Pos.CENTER);
 
                 // Đưa ImageView và các Label vào VBox
-                productBox.getChildren().addAll(productImageView, productNameButton, productPriceLabel);
+                productBox.getChildren().addAll(productImageView, productNameLabel, productPriceLabel);
 
                 // Đặt VBox vào Pane sản phẩm
                 Pane productPane = new Pane(productBox);
@@ -127,6 +145,99 @@ public class HomeController implements Initializable {
 
         } catch (SQLException e) {
             e.printStackTrace();
+        } 
+    }
+
+    private void displayProducts2() {
+
+        try {
+            connection = connect.getConnection();
+
+            String query = "SELECT ProductsName.ProductName, product.img, importgoods.price, product.categoryId, category.categoryName "
+                    + "FROM product "
+                    + "INNER JOIN ProductsName ON product.ProductNameId = ProductsName.ProductNameId "
+                    + "INNER JOIN importgoods ON importgoods.ProductNameId = product.ProductNameId "
+                    + "INNER JOIN supplier ON product.supplier_id = supplier.supplierId "
+                    + "INNER JOIN category ON product.categoryId = category.categoryId "
+                    + "WHERE product.categoryId = 9";
+
+            statement = connection.prepareStatement(query);
+
+            resultSet = statement.executeQuery();
+
+            int productSpacing = 1; // Khoảng cách giữa các sản phẩm
+            int productWidth = 190; // Chiều rộng của mỗi sản phẩm
+            int productHeight = 205; // Chiều cao của mỗi sản phẩm
+            int startX = 21; // Vị trí ban đầu theo trục X
+            int startY = 4; // Vị trí ban đầu theo trục Y
+
+            int productIndex = 0; // Chỉ số của sản phẩm
+            if (resultSet.next()) {
+                String categoryName = resultSet.getString("categoryName");
+                // Đặt giá trị categoryName cho nút Button
+                categoryId2.setText(categoryName);
+            }
+
+            while (resultSet.next()) {
+                String productName = resultSet.getString("ProductName");
+                String imagePath = resultSet.getString("img");
+                Float productPrice = resultSet.getFloat("price");
+
+                // Tạo VBox để chứa các thành phần
+                VBox productBox = new VBox();
+                productBox.setSpacing(2); // Đặt khoảng cách giữa các thành phần là 2px
+                productBox.setAlignment(Pos.CENTER);
+
+                // Tạo ImageView cho sản phẩm
+                ImageView productImageView = new ImageView();
+                productImageView.setFitWidth(140);
+                productImageView.setFitHeight(125);
+                productImageView.setPreserveRatio(true);
+                Image image = new Image("file:///C:/java2/projectJava2/" + imagePath);
+                productImageView.setImage(image);
+
+//// Tạo Label cho tên sản phẩm
+                Label productNameLabel = new Label(productName);
+                productNameLabel.setPrefWidth(150);
+                productNameLabel.setPrefHeight(25);
+                productNameLabel.setAlignment(Pos.CENTER);
+
+                // Đặt xử lý sự kiện cho Label 
+                productNameLabel.setOnMouseClicked(event -> {
+                    // Xử lý khi Label được nhấp chuột
+                });
+
+// Đưa Button và các thành phần khác vào VBox
+                // Tạo Label cho giá sản phẩm
+                Label productPriceLabel = new Label("Price: " + productPrice);
+                productPriceLabel.setPrefWidth(140);
+                productPriceLabel.setPrefHeight(25);
+                productPriceLabel.setAlignment(Pos.CENTER);
+
+                // Đưa ImageView và các Label vào VBox
+                productBox.getChildren().addAll(productImageView, productNameLabel, productPriceLabel);
+
+                // Đặt VBox vào Pane sản phẩm
+                Pane productPane = new Pane(productBox);
+                productPane.setPrefWidth(productWidth);
+                productPane.setPrefHeight(productHeight);
+
+                // Tính toán vị trí của sản phẩm dựa trên chỉ số và khoảng cách
+                int productX = startX + (productWidth + productSpacing) * productIndex;
+                int productY = startY;
+
+                // Đặt vị trí cho Pane sản phẩm
+                productPane.setLayoutX(productX);
+                productPane.setLayoutY(productY);
+
+                // Thêm Pane sản phẩm vào AnchorPane "bgV"
+                bgV1.getChildren().add(productPane);
+                scrollPane1.setContent(bgV1);
+                productIndex++; // Tăng chỉ số sản phẩm
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
         } finally {
             try {
                 if (resultSet != null) {
@@ -143,5 +254,4 @@ public class HomeController implements Initializable {
             }
         }
     }
-
 }
